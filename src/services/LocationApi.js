beab39f3b82a88d8453e2087a6fe92c2aa0e6fa5
Vinícius Pereira Polli <viniciusppolli07@gmail.com>
2024-11-28 00:52:11 -0300

    mapa arrumado

--- src/services/LocationApi.js
+++ src/services/LocationApi.js
@@ -1,27 +1,32 @@
 import axios from "axios";
 import AsyncStorage from '@react-native-async-storage/async-storage'; // Para pegar o token de autenticação
-import { useEffect } from "react";
- 
+
 // Função para enviar a localização ao banco de dados
-export const sendLocationToDatabase = ({latitude, longitude,UUID}) => {
-    useEffect (() => {
-        console.log ("id",UUID)
-    })
+export const sendLocationToDatabase = async ({ latitude, longitude, UUID }) => {
+    // Recuperar o token de autenticação do AsyncStorage
+    const token = await AsyncStorage.getItem('authToken');
+    
+    if (!token) {
+        console.log("Token de autenticação não encontrado!");
+        return;
+    }
+
+    // Dados a serem enviados
     const data = {
         latitude,
         longitude
     };
- 
+
     // Enviar a localização para o banco de dados usando a API
-    axios.post(`https://goobarapi-2.onrender.com/Bar/location/${UUID}`, data, {
-        headers: {
-            Authorization: `Bearer ${token}`, // Passando o token de autenticação
-        }
-    })
-    .then(response => {
+    try {
+        const response = await axios.post(`https://goobarapi-2.onrender.com/Bar/location/${UUID}`, data, {
+            headers: {
+                Authorization: `Bearer ${token}`, // Passando o token de autenticação
+            }
+        });
+
         console.log("Localização enviada com sucesso!", response.data);
-    })
-    .catch(error => {
+    } catch (error) {
         console.log("Erro ao enviar localização:", error.response ? error.response.data : error.message);
-    });
+    }
 };
